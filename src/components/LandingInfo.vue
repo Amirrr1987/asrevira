68805f9863b59413051087ed988587348c312db6
Amirrr1987 <Maghami.A1987@gmail.com>
2020-09-22 13:46:20 +0330

    add landing-info basic tags

---
+++ src/components/LandingInfo.vue
@@ -0,0 +1,21 @@
+<template>
+  <section class="landing-info">
+      <div class="landing-info__container">
+          <div class="landing-info__row">
+              <div class="landing-info__item">item</div>
+              <div class="landing-info__item">item</div>
+              <div class="landing-info__item">item</div>
+          </div>
+      </div>
+  </section>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>

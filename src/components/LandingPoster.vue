b473bc803d17a493a9177d02027a60372cf284f0
Amirrr1987 <Maghami.A1987@gmail.com>
2020-09-22 16:19:57 +0330

    create poster component

---
+++ src/components/LandingPoster.vue
@@ -0,0 +1,15 @@
+<template>
+  <section class="landing-poster">
+      poster
+  </section>
+</template>
+
+<script>
+export default {
+    name: 'LandingPoster',
+}
+</script>
+
+<style>
+
+</style>

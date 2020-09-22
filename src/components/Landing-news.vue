0bb94389ed4505b737dd24c203401d81d838df8a
Amirrr1987 <Maghami.A1987@gmail.com>
2020-09-22 13:48:11 +0330

    add landing-news basic tags

---
+++ src/components/Landing-news.vue
@@ -0,0 +1,21 @@
+<template>
+  <section class="landing-news">
+      <div class="landing-news__container">
+          <div class="landing-news__row">
+              <div class="landing-news__item">item</div>
+              <div class="landing-news__item">item</div>
+              <div class="landing-news__item">item</div>
+          </div>
+      </div>
+  </section>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>

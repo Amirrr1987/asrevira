dfc5abdaa26dfc171d4b08a125339850bfb3c8ba
Amirrr1987 <Maghami.A1987@gmail.com>
2020-09-22 16:26:24 +0330

    create component

---
+++ src/components/LandingTraining.vue
@@ -0,0 +1,15 @@
+<template>
+  <section class="landing-training">
+      training
+  </section>
+</template>
+
+<script>
+export default {
+    name: 'LandingTraining',
+}
+</script>
+
+<style>
+
+</style>

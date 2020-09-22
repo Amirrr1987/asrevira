52e4ab4d6ac37ac4558eec166432bdc66a406708
Amirrr1987 <Maghami.A1987@gmail.com>
2020-09-22 16:57:41 +0330

    fix social icon name add imp in footer

--- src/views/Footer.vue
+++ src/views/Footer.vue
@@ -1,12 +1,26 @@
 <template>
   <footer class="footer">
-    footer
+    <div class="footer__container">
+      <div class="footer__row">
+        <div class="footer__social">
+          <h4 class="footer__socail-title">شبکه های اجتماعی</h4>
+          <div class="footer__social-items">
+            <a href="" class=""><img src="@/assets/img/social/instagram.png" alt="" class=""></a>
+            <a href="" class=""><img src="@/assets/img/social/telgram.png" alt="" class=""></a>
+            <a href="" class=""><img src="@/assets/img/social/twitter.png" alt="" class=""></a>
+            <a href="" class=""><img src="@/assets/img/social/aparat.png" alt="" class=""></a>
+            <a href="" class=""><img src="@/assets/img/social/aparat.png" alt="" class=""></a>
+            <a href="" class=""><img src="@/assets/img/social/facebook.png" alt="" class=""></a>
+          </div>
+        </div>
+      </div>
+    </div>
   </footer>
 </template>
 
 <script>
 export default {
-  name: 'Footet',
+  name: 'Footer',
 }
 </script>
 

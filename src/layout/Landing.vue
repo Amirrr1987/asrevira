35c81cf89edfacd284307718a57ca5c57eabb56f
Amirrr1987 <Maghami.A1987@gmail.com>
2020-09-22 16:49:07 +0330

    add section componets in landingPage

--- src/layout/Landing.vue
+++ src/layout/Landing.vue
@@ -1,12 +1,26 @@
 <template>
   <main class="main landing">
-      Landing
+      <LandingInfo/>
+      <LandingNews/>
+      <LandingServices/>
+      <LandingTraining/>
   </main>
 </template>
 
 <script>
+import LandingInfo     from '@/components/LandingInfo';
+import LandingNews     from '@/components/LandingNews';
+import LandingServices from '@/components/LandingServices';
+import LandingTraining from '@/components/LandingTraining';
 export default {
     name: 'Landing',
+    components:{
+      LandingInfo,
+      LandingNews,
+      LandingServices,
+      LandingTraining,
+
+    }
 }
 </script>
 

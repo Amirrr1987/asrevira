b8c33114be5c1b8f32646755833fd257f8986338
Amirrr1987 <Maghami.A1987@gmail.com>
2020-09-22 17:01:22 +0330

    fix socail section in footer

--- src/views/Footer.vue
+++ src/views/Footer.vue
@@ -2,15 +2,15 @@
   <footer class="footer">
     <div class="footer__container">
       <div class="footer__row">
-        <div class="footer__social">
-          <h4 class="footer__socail-title">شبکه های اجتماعی</h4>
-          <div class="footer__social-items">
-            <a href="" class=""><img src="@/assets/img/social/instagram.png" alt="" class=""></a>
-            <a href="" class=""><img src="@/assets/img/social/telgram.png" alt="" class=""></a>
-            <a href="" class=""><img src="@/assets/img/social/twitter.png" alt="" class=""></a>
-            <a href="" class=""><img src="@/assets/img/social/aparat.png" alt="" class=""></a>
-            <a href="" class=""><img src="@/assets/img/social/aparat.png" alt="" class=""></a>
-            <a href="" class=""><img src="@/assets/img/social/facebook.png" alt="" class=""></a>
+        <div class="footer__social social">
+          <h4 class="socail__title">شبکه های اجتماعی</h4>
+          <div class="social__items">
+            <a href="" class="social__link"><img src="@/assets/img/social/instagram.png" alt="" class="social__item social--instagram"></a>
+            <a href="" class="social__link"><img src="@/assets/img/social/telegram.png"  alt="" class="social__item social--telegram"></a>
+            <a href="" class="social__link"><img src="@/assets/img/social/twitter.png"   alt="" class="social__item social--twitter"></a>
+            <a href="" class="social__link"><img src="@/assets/img/social/aparat.png"    alt="" class="social__item social--youtube"></a>
+            <a href="" class="social__link"><img src="@/assets/img/social/aparat.png"    alt="" class="social__item social--aparat"></a>
+            <a href="" class="social__link"><img src="@/assets/img/social/facebook.png"  alt="" class="social__item social--facebook"></a>
           </div>
         </div>
       </div>

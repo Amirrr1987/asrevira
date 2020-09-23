bf8d47e2eef754ce9858a800367d9179aaa01765
Amirrr1987 <Maghami.A1987@gmail.com>
2020-09-23 14:13:46 +0330

    fix title and gap size in md

--- src/components/LandingInfo.vue
+++ src/components/LandingInfo.vue
@@ -4,21 +4,21 @@
           <div class="landing-info__row">
               <div class="landing-info__item">
                   <img src="@/assets/img/info/security.svg" title="" alt="" class="landing-info__img">
-                  <h4 class="landing-info__title">شسبیسشی</h4>
+                  <h4 class="landing-info__title">امنیت</h4>
                   <p class="landing-info__caption">
                       لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است
                   </p>
               </div>
               <div class="landing-info__item">
                   <img src="@/assets/img/info/advantage.svg" title="" alt="" class="landing-info__img">
-                  <h4 class="landing-info__title">شسبیسشی</h4>
+                  <h4 class="landing-info__title">مزیت</h4>
                   <p class="landing-info__caption">
                       لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است
                   </p>
               </div>
               <div class="landing-info__item">
                   <img src="@/assets/img/info/easy.svg" title="" alt="" class="landing-info__img">
-                  <h4 class="landing-info__title">شسبیسشی</h4>
+                  <h4 class="landing-info__title">راحتی</h4>
                   <p class="landing-info__caption">
                       لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است
                   </p>

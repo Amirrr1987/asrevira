ef8ef45233bbf21b584b9dc990285a131010ba96
Amirrr1987 <Maghami.A1987@gmail.com>
2020-09-23 14:11:30 +0330

    change icons name & add class & style and add content

--- src/components/LandingInfo.vue
+++ src/components/LandingInfo.vue
@@ -2,9 +2,27 @@
   <section class="landing-info">
       <div class="landing-info__container">
           <div class="landing-info__row">
-              <div class="landing-info__item">item</div>
-              <div class="landing-info__item">item</div>
-              <div class="landing-info__item">item</div>
+              <div class="landing-info__item">
+                  <img src="@/assets/img/info/security.svg" title="" alt="" class="landing-info__img">
+                  <h4 class="landing-info__title">شسبیسشی</h4>
+                  <p class="landing-info__caption">
+                      لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است
+                  </p>
+              </div>
+              <div class="landing-info__item">
+                  <img src="@/assets/img/info/advantage.svg" title="" alt="" class="landing-info__img">
+                  <h4 class="landing-info__title">شسبیسشی</h4>
+                  <p class="landing-info__caption">
+                      لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است
+                  </p>
+              </div>
+              <div class="landing-info__item">
+                  <img src="@/assets/img/info/easy.svg" title="" alt="" class="landing-info__img">
+                  <h4 class="landing-info__title">شسبیسشی</h4>
+                  <p class="landing-info__caption">
+                      لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است
+                  </p>
+              </div>
           </div>
       </div>
   </section>

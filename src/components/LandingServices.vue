eb37920e83a479de3afe6da8765fe8b1ec94ad87
Amirrr1987 <Maghami.A1987@gmail.com>
2020-09-26 16:23:44 +0330

    fix services component

--- src/components/LandingServices.vue
+++ src/components/LandingServices.vue
@@ -1,6 +1,8 @@
 <template>
   <section class="landing-services">
-
+    <div class="landing-services__container">
+      <img src="@/assets/img/services/services.svg" title="" alt="" class="landing-services__img">
+    </div>
   </section>
 </template>
 

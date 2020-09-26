94fc60a8e77eb9ffcf273f4d7c7862788ac9b734
Amirrr1987 <Maghami.A1987@gmail.com>
2020-09-26 13:52:57 +0330

    v-for add json

--- src/views/Footer.vue
+++ src/views/Footer.vue
@@ -5,37 +5,20 @@
         <div class="footer__useful-links useful-links">
           <h4 class="useful-links__title">لینک های مفید</h4>
           <ul class="useful-links__items">
-            <li class="useful-links__item"><a href="" class="useful-links__link">asd</a></li>
-            <li class="useful-links__item"><a href="" class="useful-links__link">asd</a></li>
-            <li class="useful-links__item"><a href="" class="useful-links__link">asd</a></li>
-            <li class="useful-links__item"><a href="" class="useful-links__link">asd</a></li>
-            <li class="useful-links__item"><a href="" class="useful-links__link">asd</a></li>
+            <li class="useful-links__item" v-for="(link,index) in links" :key="index"><a :href="link.url" target="_blank" class="useful-links__link">{{link.text}}</a></li>
           </ul>
         </div>
         <div class="footer__about-us about-us">
           <h4 class="about-us__title">ارتباط با ما</h4>
-          <p class="about-us__item item">
-           <span class="item__title">تلفن شرکت : </span> 
-           <span class="item__content">0218421844</span>
+          <p class="about-us__item item"  v-for="(contact,index) in contacts" :key="index">
+           <span class="item__title">{{contact.title}} : </span> 
+           <span class="item__content">{{contact.content}}</span>
           </p>          
-          <p class="about-us__item item">
-           <span class="item__title">تلفن شرکت : </span> 
-           <span class="item__content">0218421844</span>
-          </p>          
-          <p class="about-us__item item">
-           <span class="item__title">تلفن شرکت : </span> 
-           <span class="item__content">0218421844</span>
-          </p>
         </div>
         <div class="footer__social social">
           <h4 class="social__title">شبکه های اجتماعی</h4>
           <div class="social__items">
-            <a href="" class="social__link"><img src="@/assets/img/social/instagram.png" alt="" class="social__img social--instagram"></a>
-            <a href="" class="social__link"><img src="@/assets/img/social/telegram.png"  alt="" class="social__img social--telegram"></a>
-            <a href="" class="social__link"><img src="@/assets/img/social/twitter.png"   alt="" class="social__img social--twitter"></a>
-            <a href="" class="social__link"><img src="@/assets/img/social/aparat.png"    alt="" class="social__img social--youtube"></a>
-            <a href="" class="social__link"><img src="@/assets/img/social/aparat.png"    alt="" class="social__img social--aparat"></a>
-            <a href="" class="social__link"><img src="@/assets/img/social/facebook.png"  alt="" class="social__img social--facebook"></a>
+            <a :href="social.imgUrl" class="social__link"  v-for="(social,index) in socials" :key="index"><img :src="social.imgSrc" alt="" class="social__img social--instagram"></a>
           </div>
         </div>
       </div>
@@ -50,7 +33,89 @@ export default {
    data() {
     return {
       copyright: '© تمامی حقوق برای عصر ویرا محفوظ است',
-      
+      socials:[
+        {
+          imgUrl: 'instagram.com',
+          imgSrc: 'src/assets/img/social/instagram.png'
+        },
+        {
+          imgUrl: 'telegram.com',
+          imgSrc: 'src/assets/img/social/telegram.png'
+        },
+        {
+          imgUrl: 'twitter.com',
+          imgSrc: 'src/assets/img/social/twitter.png'
+        },
+        {
+          imgUrl: 'aparat.com',
+          imgSrc: 'src/assets/img/social/aparat.png'
+        },
+        {
+          imgUrl: 'instagram.com',
+          imgSrc: 'src/assets/img/social/instagram.png'
+        },
+        {
+          imgUrl: 'facebook.com',
+          imgSrc: 'https://picsum.photos/id/237/200/300'
+        },
+        {
+          imgUrl: 'facebook.com',
+          imgSrc: 'https://picsum.photos/id/237/200/300'
+        },
+        {
+          imgUrl: 'facebook.com',
+          imgSrc: 'https://picsum.photos/id/237/200/300'
+        },
+        {
+          imgUrl: 'facebook.com',
+          imgSrc: 'src/assets/img/social/facebook.png'
+        },
+      ]
+      ,
+      links:[
+        {
+          text: 'سوالات',
+          url: 'www.amir.ir'
+        },
+        {
+          text: 'سوالات',
+          url: 'www.amir.ir'
+        },
+        {
+          text: 'سوالات',
+          url: 'www.amir.ir'
+        },
+        {
+          text: 'سوالات',
+          url: 'www.amir.ir'
+        },
+        {
+          text: 'سوالات',
+          url: 'www.amir.ir'
+        },
+        {
+          text: 'سوالات',
+          url: 'www.amir.ir'
+        },
+      ],
+      contacts:[
+        {
+          title: 'تلفن شرکت',
+          content: '02188776655'
+        },
+        {
+          title: 'تلفن شرکت',
+          content: '02188776655'
+        },
+        {
+          title: 'تلفن شرکت',
+          content: '02188776655'
+        },
+        {
+          title: 'تلفن شرکت',
+          content: '02188776655'
+        },
+      ]
     }
    }
 }
